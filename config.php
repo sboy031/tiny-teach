455bbca2c53899b2f043f298329be061e8b7163a
tinyteach <sam.boychuk@gmail.com>
2017-03-06 16:28:19 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$tinyteach_from_email = "example@example.com"; //from domain at host
-$tinyteach_to_email = 'example@example.com';
-$tinyteach_submission_msg = 'Submit your answers. Please enter a valid email.';
-$tinyteach_nonsubmission_msg = 'Return to the Tiny Teach online index.';
-$tinyteach_askforemail = true;
-$tinyteach_circular_slides = true;
-$tinyteach_logo = "./assets/ttlogo.png";
-$tinyteach_title = "Welcome to Tiny Teach";
+$tinyteach_from_email = "example@example.com";  //mail uses this from address (must be valid domain on your host)
+$tinyteach_to_email = 'example@example.com';    //address submitted data is sent to
+$tinyteach_submission_msg = 'Submit your answers. Please enter a valid email.'; //Submission Prompt Text
+$tinyteach_nonsubmission_msg = 'Return to the Tiny Teach online index.';        //Prompt Text when Submission disabled
+$tinyteach_askforemail = true;                  //True if submissions are required, false to disable
+$tinyteach_circular_slides = true;              //True to enable circular borders on slides
+$tinyteach_logo = "./assets/ttlogo.png";        //Location of Index page Logo
+$tinyteach_title = "Welcome to Tiny Teach";     //Site title
 ?>
